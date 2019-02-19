3cd825ead93b701537175a1cde54bc8b18533ac8
shalini1995 <34934102+shalini1995@users.noreply.github.com>
2019-02-19 17:58:13 +0530

    welcome mwssage

--- server/server.js
+++ server/server.js
@@ -13,6 +13,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user conncted');
 
+socket.emit('newMessage', {
+  from:'admin',
+  text:'Welcome to chat app',
+  createdAt:new Date().getTime()
+});
+socket.broadcast.emit('newMessage',{
+  from:'admin',
+  text:'new User has been joined',
+  createdAt:new Date().getTime()
+});
+
+ // socket.on('createMessage', (message) => {
+ //   console.log(message);
+ //   io.emit('newMessage',{
+ //     text:message.text,
+ //     from:message.from,
+ //     createdAt:new Date().getTime().toString()
+ //   });
+ //
+ //
+ // });
+
+
   socket.on('disconnect', () => {
     console.log('user was disconnected');
   });
