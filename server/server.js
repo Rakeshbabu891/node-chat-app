e8ca79791fb5626bca2644aef0dc882e6c750fa8
shalini1995 <34934102+shalini1995@users.noreply.github.com>
2019-02-19 16:00:48 +0530

    socket io.on

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 var publicPath = path.join(__dirname + '../../public');
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log('new user conncted');
 
+  socket.on('disconnect', () => {
+    console.log('user was disconnected');
+  });
+});
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server has been started successfully');
 });
